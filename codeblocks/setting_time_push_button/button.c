f4a2e77600fde241a1165d5cb9a4aa7c51bf8907
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2019-04-10 18:02:29 +0100

    Add 3 different states for the setting time button

--- codeblocks/setting_time_push_button/button.c
+++ codeblocks/setting_time_push_button/button.c
@@ -1,12 +1,98 @@
-// Setting time push button behavior
-
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/time.h>
 #include <wiringPi.h>
 
-#include <stdio.h>
 
-const int INPUT_PIN = 0;
+// What GPIO input are we using?
+//	This is a wiringPi pin number
 
-int listenPushButton(int* isSettingTime, int* isSettingHours, int* isSettingMinutes){
-  waitForInterrupt(INPUT_PIN, -1);
-  return 1;
+#define	BUTTON_PIN	7
+
+// globalCounter:
+//	Global variable to count interrupts
+//	Should be declared volatile to make sure the compiler doesn't cache it.
+
+struct timeval time_pushed, time_released;
+static int isSettingTime = 0;
+static int isSettingHours = 0;
+static int isSettingMinutes = 0;
+
+static const int TIME_THRESHOLD = 3;
+
+/*
+ * myInterrupt:
+ *********************************************************************************
+ */
+
+void myInterrupt (void){
+	// The time is not being set
+	if (isSettingTime == 0){
+		// Up
+		if (digitalRead(BUTTON_PIN) == LOW)
+			gettimeofday(&time_pushed, NULL);
+		// Down
+		else{
+			gettimeofday(&time_released, NULL);
+			double time_pushing = time_released.tv_sec - time_pushed.tv_sec;
+			if (time_pushing >= TIME_THRESHOLD){
+				printf("Set Time\n");
+				isSettingTime = 1;
+				isSettingHours = 1;
+			}
+		}
+	}
+	else{
+		if (digitalRead(BUTTON_PIN) == LOW){
+			if (isSettingMinutes){
+				printf("Time is set\n");
+				isSettingTime = 0;
+				isSettingHours = 0;
+				isSettingMinutes = 0;
+				// Reset the timer
+				gettimeofday(&time_pushed, NULL);
+				gettimeofday(&time_pushed, NULL);
+
+			}
+			if (isSettingHours){
+				printf("Now set the minutes\n");
+				isSettingHours = 0;
+				isSettingMinutes = 1;
+			}
+		}
+	}
 }
+
+
+/*
+ *********************************************************************************
+ * main
+ *********************************************************************************
+ */
+
+int main (void)
+{
+
+  if (wiringPiSetup () < 0)
+  {
+    fprintf (stderr, "Unable to setup wiringPi: %s\n", strerror (errno)) ;
+    return 1 ;
+  }
+
+  if (wiringPiISR (BUTTON_PIN, INT_EDGE_BOTH, &myInterrupt) < 0)
+  {
+    fprintf (stderr, "Unable to setup ISR: %s\n", strerror (errno)) ;
+    return 1 ;
+  }
+
+
+  for (;;)
+  {
+	continue;
+  }
+
+  return 0 ;
+}
+
